cb8e27633090a0d45ab1a82574fb6d77b1226ad7
Tatiana-Tsymbaliuk <114348739+Tatiana-Tsymbaliuk@users.noreply.github.com>
2023-03-18 19:02:39 +0000

    Deploying to gh-pages from @ Tatiana-Tsymbaliuk/goit-js-hw-09@271950e4b48f5703c9505dbe40954f6e40ba5b81 🚀

--- 03-promises.24e40777.js
+++
@@ -1,2 +0,0 @@
-!function(){var o=document.querySelector("button");console.log(o)}();
-//# sourceMappingURL=03-promises.24e40777.js.map
--- 03-promises.d9aa8e6d.js
+++
@@ -1,2 +0,0 @@
-const o=document.querySelector("button");console.log(o);
-//# sourceMappingURL=03-promises.d9aa8e6d.js.map
